4c73e0353b29b01a725a27d94ddfa0c5131b45b4
whyj107 <60024292+whyj107@users.noreply.github.com>
2021-07-02 23:41:31 +0900

    kata6

--- 20210702_up AND down.py
+++ 20210702_up AND down.py
@@ -3,25 +3,41 @@
 
 # 나의 풀이
 def arrange(strng):
+    if strng == '': return ''
     strng = strng.split()
+    result = []
     pre = strng[0]
-    result = [pre]
-
-    for i in range(1, len(strng)-1):
-        if len(pre) <= len(strng[i]) and len(strng[i]) >= len(strng[i+1]):
-            result.append(pre)
-            pre = strng[i-1]
+    for i in range(1, len(strng)):
+        if (len(pre) > len(strng[i]) and i%2 != 0) or (len(pre) < len(strng[i]) and i%2 == 0):
+            result.append(strng[i])
         else:
-            result.append(strng[i+1])
-            pre = strng[i+1]
-    print(result)
-    return '\n'
+            result.append(pre)
+            pre = strng[i]
+    result.append(pre)
+
+    return ' '.join([r.lower() if idx%2 == 0 else r.upper() for idx, r in enumerate(result)])
+
+# 다른 사람의 풀이
+def arrange1(strng):
+    words = strng.split()
+    for i in range(len(words)):
+        words[i:i+2] = sorted(words[i:i+2], key=len, reverse=i%2)
+        words[i] = words[i].upper() if i%2 else words[i].lower()
+    return ' '.join(words)
 
 def testing(actual, expected):
     print(actual, expected)
 
+testing(arrange("after be arrived two my so"),
+        "be arrived two after my so")
+
+"""
 testing(arrange("who hit retaining The That a we taken"),
         "who RETAINING hit THAT a THE we TAKEN")  # 3
-testing(arrange("on I came up were so grandmothers"), "i CAME on WERE up GRANDMOTHERS so")  # 4
-testing(arrange("way the my wall them him"), "way THE my WALL him THEM")  # 1
-testing(arrange("turn know great-aunts aunt look A to back"), "turn GREAT-AUNTS know AUNT a LOOK to BACK")  # 2
+testing(arrange("on I came up were so grandmothers"),
+        "i CAME on WERE up GRANDMOTHERS so")  # 4
+testing(arrange("way the my wall them him"),
+        "way THE my WALL him THEM")  # 1
+testing(arrange("turn know great-aunts aunt look A to back"),
+        "turn GREAT-AUNTS know AUNT a LOOK to BACK")  # 2
+"""
