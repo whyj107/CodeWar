8827b2834b890d98978f6a13e0cb4b8f2fc1501b
whyj107 <60024292+whyj107@users.noreply.github.com>
2021-07-09 08:46:08 +0900

    kata6
    아직 제작 중

---
+++ 20210709_Squared Spiral #1.py
@@ -0,0 +1,14 @@
+# Squared Spiral #1
+# https://www.codewars.com/kata/60a38f4df61065004fd7b4a7/train/python
+
+# 나의 풀이
+def squared_spiral(n):
+    # 알고리즘 생성 중...
+    return
+
+
+print(squared_spiral(0), (0, 0))
+print(squared_spiral(6), (-1, -1))
+print(squared_spiral(10), (2, 0))
+print(squared_spiral(12), (2, 2))
+print(squared_spiral(30), (3, 3))
