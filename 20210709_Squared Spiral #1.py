c07017b4400f39e7c1c868d37feb6b84eddd1ddb
whyj107 <60024292+whyj107@users.noreply.github.com>
2021-07-09 22:13:53 +0900

    kata6
    생각보다 시간이 오래 걸린 문제이다
    짝수들의 합 : n*n+n

--- 20210709_Squared Spiral #1.py
+++ 20210709_Squared Spiral #1.py
@@ -2,13 +2,32 @@
 # https://www.codewars.com/kata/60a38f4df61065004fd7b4a7/train/python
 
 # 나의 풀이
+import math
 def squared_spiral(n):
-    # 알고리즘 생성 중...
-    return
+    if n == 0: return (0, 0)
+    idx = round(n**0.5)
+    start = math.ceil(idx / 2)
+    n_ = -1
+    if idx % 2 == 0:
+        start *= -1
+        n_ = 1
+    tmp = idx**2 + idx - n
+    if tmp < idx:
+        return (start, start+n_*tmp)
+    else:
+        return (start+n_*(tmp-idx), start+n_*idx)
 
+# 다른 사람의 풀이
+def squared_spiral1(n):
+    b = round(n**.5)
+    e = (b//2+b%2-max(0, b**2-n), -(b//2) + max(0, n-b**2))
+    return e if b % 2 else (-e[0], -e[1])
 
-print(squared_spiral(0), (0, 0))
-print(squared_spiral(6), (-1, -1))
-print(squared_spiral(10), (2, 0))
-print(squared_spiral(12), (2, 2))
-print(squared_spiral(30), (3, 3))
+# print(squared_spiral(0), (0, 0))
+# print(squared_spiral(6), (-1, -1))
+# print(squared_spiral(10), (2, 0))
+# print(squared_spiral(12), (2, 2))
+# print(squared_spiral(30), (3, 3))
+# print(squared_spiral(50264), (-112, 24))
+# print(squared_spiral(50263), (-112, 25))
+print(squared_spiral(25155), (-46, -79))
