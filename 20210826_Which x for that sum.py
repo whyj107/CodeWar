7bb0ad2c87c38ec2af7479c50ece3d684e30aa68
whyj107 <60024292+whyj107@users.noreply.github.com>
2021-08-26 11:29:27 +0900

    kata5
    이건 수학 문제 그래도 오랜만에 풀어서 재밌었다.
    적분까지 생각했는데 그렇게까지 갈 필요 없었다.

--- 20210826_Which x for that sum.py
+++ 20210826_Which x for that sum.py
@@ -3,13 +3,11 @@
 
 # 나의 풀이
 def solve(m):
-    # U(n, x) = x + 2x**2 + 3x**3 + ... + nx**n
-    # solve(2.0) => U(무한, 0.5)
-    # m = x/((1-x)**2)
-    # 적분해야할 것 같은데 나중에 다시 풀어보자
-    result = 0
-    for i in range(1, m+1):
-        result += (i*0.6096**i)
-    return result
+    # m = x + 2x**2 + 3x**3 + ... + nx**n
+    # m = x/(x-1)**0.5
+    # mx**2 + 2(-m-0.5)x + m = 0
+    # 근의 공식 사용 / use quadratic formula
+    # x range 0<x<1
+    return 1+0.5/m - ((m+0.25)**0.5)/m
 
 print(solve(100))
