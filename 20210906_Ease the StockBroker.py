5ed84d2df920eda57bc41e30d5ba4b8d8c0ac284
whyj107 <60024292+whyj107@users.noreply.github.com>
2021-09-06 07:23:46 +0900

    kata6
    정규표현식이 짧게 코딩하게 좋은 것 같다.
    하지만 문제 포맷이 좀... 그런듯

--- 20210906_Ease the StockBroker.py
+++ 20210906_Ease the StockBroker.py
@@ -24,6 +24,21 @@ def balance_statement(lst):
             err.append(' '.join(tmp) + ' ;')
     return f'Buy: {round(buy)} Sell: {round(sell)}' + (f'; Badly formed {len(err)}: ' + ''.join(err) if len(err) > 0 else '')
 
+# 다른 사람의 풀이
+import re
+def balance_statement1(lst):
+    bad_formed, prices = [], {'B':0, 'S':0}
+    for order in filter(None, lst.split(', ')):
+        if not re.match('\S+ \d+ \d*\.\d+ (B|S)', order):
+            bad_formed.append(order + ' ;')
+        else:
+            _, quantity, price, status = order.split()
+            prices[status] += int(quantity) * float(price)
+    ret = "Buy: %.0f Sell: %.0f" % (prices['B'], prices['S'])
+    if bad_formed:
+        ret += "; Badly formed %d: %s" % (len(bad_formed), ''.join(bad_formed))
+    return ret
+
 """
 print(
     balance_statement("ZNGA 1300 2.66 B, "
