a106b5b76d03567eaa3f5134dcf7d23d7a90861d
whyj <60024292+whyj107@users.noreply.github.com>
2022-06-22 10:49:00 +0900

    i-1, i, i+1, for, while, re
    while과 정규표현식으로 푸는 방법 익혀두면 좋을 듯

--- 20220622_SevenAte9.py
+++ 20220622_SevenAte9.py
@@ -5,14 +5,17 @@
 def seven_ate9(str_):
     result = str_[0]
     for i in range(1, len(str_)-1):
-        print(str_[i-1], str_[i+1], str_[i])
         if str_[i-1] == str_[i+1] == '7' and str_[i]=='9':
             continue
         result += str_[i]
     return result + str_[-1]
 
 # 다른 사람의 풀이
-def seven_ate91(str_):
+def seven_ate9_1(str_):
     while "797" in str_:
         str_ = str_.replace("797", "77")
     return str_
+
+import re
+def seven_ate9_2(str_):
+    return re.sub(r'79(?=7)', r'7', str_)
