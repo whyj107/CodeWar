60e190890749aa8c04fac5a4c6cc3f3535936df3
whyj <60024292+whyj107@users.noreply.github.com>
2022-07-26 14:44:55 +0900

    EZ

---
+++ 20220726_Area of an arrow.py
@@ -0,0 +1,10 @@
+# Area of an arrow
+# https://www.codewars.com/kata/589478160c0f8a40870000bc/train/python
+
+# 나의 풀이
+def arrow_area(a, b):
+    return a*b*0.25
+
+print(arrow_area(4, 2), 2)
+print(arrow_area(7, 6), 10.5)
+print(arrow_area(25, 25), 156.25)
