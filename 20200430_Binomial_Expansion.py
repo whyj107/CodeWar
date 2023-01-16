a580338e0727ae608264a82e22c016cd61879a21
whyj <60024292+whyj107@users.noreply.github.com>
2023-01-16 15:09:42 +0900

    Update 20200430_Binomial_Expansion.py

--- 20200430_Binomial_Expansion.py
+++ 20200430_Binomial_Expansion.py
@@ -27,71 +27,69 @@
 
 # My Code
 def expand(expr):
+    # 괄호 없애고 ^로 나누어서 분리
     tmp = expr.replace('(', '').replace(')', '').split('^')
-    if tmp[1] == str(0):
-        return str(1)
-    elif tmp[1] == str(1):
-        return tmp[0]
+    # 지수가 0이면 '1', 1이면 원래 값 반환
+    if tmp[1] == '0': return '1'
+    elif tmp[1] == '1': return tmp[0]
+
+    # 지수가 1보다 크면 계산
     else:
-        c = int(tmp[1])
-        tmp_b = [[] for _ in range(c + 1)]
-        tmp_b[0] = [1, 1]
-        for i in range(1, len(tmp_b)):
-            tmp_b[i].append(1)
-            for j in range(i-1):
-                tmp_b[i].append(tmp_b[i-1][j]+tmp_b[i-1][j+1])
-            tmp_b[i].append(1)
-        # [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]
-        # print(tmp_b[-1])
+        # (ax+b)^n
+        # 지수 저장
+        n = int(tmp[1])
 
-        a = 1
-        x = ''
-        b = 1
+        # 지수 값에 따라서 달라지는 곱해지는 계수 및 상수 구하는 계산
+        # : (x+1)^2 = x^2+2x+1 : [1, 2, 1]
+        # 계수 : coefficient
+        cs = [1, 1]
+        for i in range(n + 1):
+            cs = [1] + [cs[j]+cs[j+1] for j in range(i-1)] + [1]
 
-        if tmp[0].count('-') == 2:
-            tmp[0] = tmp[0].split('-')
-            if tmp[0][0] == '':
-                a = -1
-            else:
-                a = int(tmp[0][0])
+        # x, a와 b 구하기 구하기
+        a, x, b = 0, '', 0
+        for idx, i in enumerate(tmp[0]):
+            if i.isalpha():
+                x = i
+                a = 1 if tmp[0][:idx] == '' else (-1 if tmp[0][:idx] == '-' else int(tmp[0][:idx]))
+                b = int(tmp[0][idx+1:])
+                break
 
-            x = tmp[0][1]
-            b = int(tmp[0][2])
+        # 계산하기
+        result = ''
+        for idx, c in enumerate(cs):
+            # 지수
+            new_n = n - idx
+            # 계수
+            new_c = c * pow(a, new_n) * pow(b, idx)
 
-        elif tmp[0].count('-') == 1:
-            if tmp[0].count('+') == 1:
-                tmp[0] = tmp[0].split('+')
-                a = int(tmp[0][0][:-1])
-                x = tmp[0][0][-1]
-                b = int(tmp[0][1])
-            else:
-                tmp[0] = tmp[0].split('-')
-                if len(tmp[0][0]) > 1:
-                    a = int(tmp[0][0][:-1])
-                else:
-                    a = 1
-                x = tmp[0][0][-1]
-                b = - int(tmp[0][1])
-        else:
-            tmp[0] = tmp[0].split('+')
-            if len(tmp[0][0]) > 1:
-                a = int(tmp[0][0][:-1])
-            else:
-                a = 1
-            x = tmp[0][0][-1]
-            b = int(tmp[0][1])
+            # 계수 1, 지수 0이면 1표현, 나머지는 계수
+            gyesu = '1' if new_c == 1 else ('-' if new_c == -1 else str(new_c))
+            # 지수가 1이면 x만 표현, 나머지는 x+^지수 표현
+            jisu = x + ('' if new_n == 1 else f'^{new_n}')
 
-        answer = ''
-        cnt = int(tmp[1])
-        for i in range(len(tmp_b[-1])):
-            a_ = tmp_b[-1][i] * pow(a, cnt) * pow(b, c-cnt)
-            if a_ == 1:
-                print(x + '^' + str(cnt))
-            else:
-                print(str(a_) + x + '^' + str(cnt))
-            cnt -= 1
+            # 부호 + 계수 + 지수
+            # 부호 : 계수가 0보다 작거나 idx가 영 : ''/그 외 '+'
+            # 계수 : 지수!=0 and 계수==1 : ''/그 외 : 계수
+            # 지수 : 지수==0 : ''/그 외 : 지수
+            result += ('' if new_c<0 or idx==0 else '+')+\
+                      ('' if new_n !=0 and new_c == 1 else gyesu) + \
+                      ('' if new_n == 0 else jisu)
+    return result
 
-    return '--------------'
+import re
+P = re.compile(r'\((-?\d*)(\w)\+?(-?\d+)\)\^(\d+)')
+def expand_re(expr):
+    a, v, b, e = P.findall(expr)[0]
+    if e == '0': return '1'
+    o = [int(a != '-' and a or a and '-1' or '1'), int(b)]
+    e, p = int(e), o[:]
+    for _ in range(e - 1):
+        p.append(0)
+        p = [o[0] * coef + p[i - 1] * o[1] for i, coef in enumerate(p)]
+    res = '+'.join(f'{coef}{v}^{e - i}' if i != e else str(coef) for i, coef in enumerate(p) if coef)
+    return re.sub(r'\b1(?=[a-z])|\^1\b', '', res).replace('+-', '-')
+
 
 if __name__=='__main__':
     # print(expand("(x-1)^0"))
