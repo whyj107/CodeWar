4802d146d04c23d9952755e82f61c2835d525ecf
whyj <60024292+whyj107@users.noreply.github.com>
2023-07-12 08:08:22 +0900

    replace, count

---
+++ 20230712_How many urinals are free.py
@@ -0,0 +1,14 @@
+# How many urinals are free?
+# https://www.codewars.com/kata/5e2733f0e7432a000fb5ecc4/train/python
+
+# 나의 풀이
+def get_free_urinals(urinals):
+    if '11' in urinals: return -1
+    return urinals.replace('10', '1').replace('01', '1').replace('00','0').count('0')
+
+def get_free_urinals1(urinals):
+    return -1 if '11' in urinals else sum(((len(l)-1)//2 for l in f'0{urinals}0'.split('1')))
+
+def get_free_urinals2(urinals):
+    if urinals.count('11'): return -1
+    return urinals.replace('10', '1').replace('01', '1').replace('00','0').count('0')
