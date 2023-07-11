5eb6992d17493bf4cfde43c94608cf2c2867c018
whyj <60024292+whyj107@users.noreply.github.com>
2023-07-12 08:08:54 +0900

    Add files via upload

--- 20230712_How many urinals are free.py
+++ 20230712_How many urinals are free.py
@@ -6,6 +6,7 @@ def get_free_urinals(urinals):
     if '11' in urinals: return -1
     return urinals.replace('10', '1').replace('01', '1').replace('00','0').count('0')
 
+# 다른 사람의 풀이
 def get_free_urinals1(urinals):
     return -1 if '11' in urinals else sum(((len(l)-1)//2 for l in f'0{urinals}0'.split('1')))
 
